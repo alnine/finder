f43e5d1340d5f65a2a7f7390abc88d6652688050
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-11-27 00:36:08 +0200

    Refactoring: session handling with HOC

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { Navigation } from "../Navigation";
@@ -11,48 +11,25 @@ import { Account } from "../Account";
 import { Admin } from "../Admin";
 
 import * as ROUTES from "../../constants/routes";
-import { withFirebase } from "../Firebase";
+import { withAuthentication } from "../Session";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navigation />
 
-    this.state = {
-      authUser: null
-    };
-  }
+        <hr />
 
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser} />
-
-          <hr />
-
-          <Route exact path={ROUTES.LANDING} component={Landing} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-          <Route path={ROUTES.SIGN_IN} component={SignIn} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-          <Route path={ROUTES.HOME} component={Home} />
-          <Route path={ROUTES.ACCOUNT} component={Account} />
-          <Route path={ROUTES.ADMIN} component={Admin} />
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default withFirebase(App);
+        <Route exact path={ROUTES.LANDING} component={Landing} />
+        <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        <Route path={ROUTES.SIGN_IN} component={SignIn} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.ACCOUNT} component={Account} />
+        <Route path={ROUTES.ADMIN} component={Admin} />
+      </div>
+    </Router>
+  );
+};
+export default withAuthentication(App);
